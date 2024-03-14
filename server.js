b5c3a66c93e66b193bd35692137a58cc08261fc1
HarishKarthickS <harishskarthick.s@gmail.com>
2024-03-14 14:31:00 +0530

    Updating Backend

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
+const { startDatabase, isConnected } = require('./db/database');
+const {getRouter, postRouter, putRouter, deleteRouter}=require('./routes/CRUDIPL.routes')
 const app = express()
-
+app.use(bodyParser.json());
+app.use(express.json())
+app.use('/',getRouter);
+app.use('/', postRouter);
+app.use('/', putRouter);
+app.use('/', deleteRouter);
 app.get('/', (req, res) => {
-  res.send('Welcome to the amazing world of cricket')
+  res.send({message:'Welcome to the amazing world of cricket'})
 })
 app.get('/home', (req, res) => {
-    res.send('Welcome to the amazing world of cricket')
+  res.json({
+    message: isConnected() ? 'Database is connected' : 'Disconnected from database'
+  })
   })
 app.get('/ping',(req,res)=>{
-    res.send('pong!, Welcome to the amazing world of cricket')
+    res.send({message:'pong!, Welcome to the amazing world of cricket'})
 })
 
-app.listen(3000,(req,res)=>{
+startDatabase()
+.then(()=>{
+  app.listen(3000,async()=>{
     console.log('Starting Server ....🚀')
     console.log('Server started running on port 3000 🏃‍♂️')
+})
 })
